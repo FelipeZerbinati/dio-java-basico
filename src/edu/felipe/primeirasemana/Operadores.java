3f146a127a4402b4f275f99abbbd64bd94c6e58f
FelipeZerbinati <fezerba@gmail.com>
2024-08-27 22:59:03 -0300

    Aula 27 08 2024 - Operadores

---
+++ src/edu/felipe/primeirasemana/Operadores.java
@@ -0,0 +1,68 @@
+package edu.felipe.primeirasemana;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+        /*String nomeCompleto = "LINGUAGEM" + "JAVA";
+		
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+
+        System.out.println(nomeCompleto);
+
+        int numero = 5;
+        
+        numero = - numero;
+        System.out.println(- numero);
+        System.out.println(numero);
+
+        // numero = + numero; // errado para tornar positivo
+        numero = numero * -1; // correto para tornar positivo
+        System.out.println(numero);
+
+        int numero = 5;
+        //x repeticao
+        numero = numero + 1;
+        numero++;
+
+        System.out.println(numero);
+
+        boolean variavel = true;
+        System.out.println(!variavel);
+        variavel = !variavel;
+        System.out.println(variavel);
+
+
+        int a, b;
+        String resultado;
+
+        a = 5;
+        b = 6;
+
+        // EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        if(a==b)
+        resultado = "verdadeiro";
+        else
+        resultado = "falso";
+        
+
+        //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+        //String resultado = a==b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);*/
+    }
+}
